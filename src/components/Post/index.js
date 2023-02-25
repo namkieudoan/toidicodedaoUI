ee9f77086fc9d22c40ccdbb2c0c29fa26942b49e
kieudoannam <kieunam2808@gmail.com>
2023-02-25 13:02:33 +0700

    updates files +  loading icon

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,9 +1,36 @@
+import classNames from "classnames/bind";
+import {Link} from "react-router-dom";
 
+import styles from "./Post.module.scss";
+
+const cx = classNames.bind(styles);
 
 function Post() {
-    return(
-        <h1>Post Page</h1>
-    );
+    return<div className={cx("post")}>
+            <h2>Post Name</h2>
+            
+            {/* <Link  className={cx("post-image")} to={post._id}>
+                <img src={post.images[0]}  alt="img-post"/>
+            </Link>
+            <div className={cx("container")}>
+                <div className={cx("post-header")}>
+                    <span className={cx("entry-category")}>{post.category}</span>
+                    <h2 className={cx("entry-title")}><Link to={post._id}>{post.title} </Link></h2>
+                    <span className={cx("entry-meta")}>
+                        <span> {post.publishedDate}</span>
+                        <span> 62 comment </span>
+                    </span>
+                </div>
+                <div className={cx("post-content")}>
+                    <p>{post.content}</p>
+                        
+                </div>
+                <div className={cx("post-direct")}>
+                    <Link  className={cx("post-direct-link")} to={post._id}>- Continute Reading -</Link>
+                    <span>{post.tags}</span>
+                </div>
+            </div> */}
+        </div>
 }
 
 export default Post;
--- src/pages/Blog/index.js
+++ src/pages/Blog/index.js
@@ -1,33 +1,43 @@
 import classNames from "classnames/bind";
 import {Link} from "react-router-dom";
 import { useEffect, useState } from "react";
+import axios from "axios";
 
 import styles from "./Blog.module.scss";
-import axios from "axios";
+import { LoadingIcon } from "../../components/icons";
 
 const cx = classNames.bind(styles);
 
-
-
 function Blog() {
-    const [postList, setPostList] = useState([])
+    const [postList, setPostList] = useState([]);
+    const [loading, setLoading] = useState(false);
 
-    useEffect(()=> { 
+    useEffect(()=> {
+        setLoading(true); 
     async function getAllPost(){
         const res = await axios.get("http://localhost:8000/v1/post");
         return res;
     };
         getAllPost()
-            .then((res)=> setPostList(res.data))
-            .catch((err)=>console.log(err));
+            .then((res)=> {
+                setPostList(res.data);
+                setLoading(false);
+            })
+            .catch((err)=>{
+                console.log(err);
+                setLoading(false);
+            });
     },[]);
 
     return(
         <div className={cx("wrapper")} >
-            
-            {postList.map((post)=>{ 
-            return <article className={cx("post")}>
-                    <Link  className={cx("post-image")} to={post._id}>
+
+            {loading && <div className={cx("loading")}><LoadingIcon/></div>}
+
+            {postList.map((post,id)=>{ 
+            return( 
+                <article className={cx("post")} key={id}>
+                    <Link  className={cx("post-image")} to={post._id} >
                         <img src={post.images[0]}  alt="img-post"/>
                     </Link>
                     <div className={cx("container")}>
@@ -49,6 +59,8 @@ function Blog() {
                         </div>
                     </div>
                 </article>
+                )
+
             })}
             
 
